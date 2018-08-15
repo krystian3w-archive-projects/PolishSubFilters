937b2cd4b8573d2a2c23b66489fa31d272e9907c
hawkeye116477 <hawkeye116477@gmail.com>
2018-08-15 22:02:57 +0200

    Fix script

--- scripts/version_checksum_update_without_templates_auto_Travis.sh
+++ scripts/version_checksum_update_without_templates_auto_Travis.sh
@@ -5,6 +5,6 @@ sciezka=$(dirname "$0")
 
 cd $sciezka/..
 
-ost_zmieniony_plik=$(git diff -z --name-only -- '*.txt' -- ':!/internal-filters/zewnetrzne_listy.txt'| xargs -0)
+ost_zmieniony_plik=$(git diff-tree --no-commit-id --name-only -r master -- '*.txt' -- ':!/internal-filters/zewnetrzne_listy.txt'| xargs -0)
 
 $sciezka/version_checksum_update_without_templates_Travis.sh $ost_zmieniony_plik
