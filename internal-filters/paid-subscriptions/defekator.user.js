500c7241c89ca7f92d9d73f74f333eef7d02a1b6
Mtq <5884000+KonoromiHimaries@users.noreply.github.com>
2019-10-06 08:09:19 +0200

    Update defekator.user.js

--- internal-filters/paid-subscriptions/defekator.user.js
+++ internal-filters/paid-subscriptions/defekator.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Defekator
 // @namespace    Def
-// @version      0.0
-// @description  Ochrona przed przekierowaniem na super pułapki
+// @version      0.1
+// @description  Ochrona przed przekierowaniem na skomplikowane pułapki
 // @author       KonoromiHimaries
 // @match        *://*.test.pl/*
 // @grant        none
 // @run-at       document-start
 // @user-agent   Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://github.com/KonoromiHimaries/PolishSubFilters/raw/master/internal-filters/paid-subscriptions/defekator.user.js
+// @downloadURL  https://github.com/KonoromiHimaries/PolishSubFilters/raw/master/internal-filters/paid-subscriptions/defekator.user.js
 // ==/UserScript==
 Object.defineProperty(navigator, 'userAgent', {
     value: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
